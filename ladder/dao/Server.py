95a7a9321c497a21c40fa333a6b7dd4dd7025164
liuyk <lyk01024@163.com>
2018-05-03 00:29:56 +0800

    up

--- ladder/dao/Server.py
+++ ladder/dao/Server.py
@@ -1,59 +1,160 @@
-
 import json
+from SQLOper import *
+from ladder.lib.RetMsg import *
+from ladder.lib.Logger import Logger
+
+logger = Logger("server").getlog()
+
 
 class ServerDao:
-
     def __init__(self):
-
-        pass
-
-
-    # 同步数据库与服务器上记录密码
-    def checkPassword(self):
-        # TODO
-        pass
+        self.table_name = "tbl_server"
+        self.select_all_which = "server_id,port,key,server_status"
 
 
-    #修改过期的密码
-    def updatePassword(self):
-        # TODO
-        pass
 
-    #插入新主机
-    def insertNewServer(self):
-        # TODO
+    def countUserDB(self, server_id):
 
+        sql = "select count(*) from %s where server_id = '%s' " % (self.table_name, server_id)
+        sqlOper = SQLOper()
+        count = sqlOper.executeSql(sql)
+        return count[0][0]
 
-        pass
+    def insertServer2DB(self, server):
+        logger.info("enter insertBalance2DB")
+        user_id = server.data["user_id"]
+
+        if self.countUserDB(user_id) == 0:
+
+            sqlOper = SQLOper()
+            sqlRes = sqlOper.executeInsertSql(self.table_name, balance.pattern, balance.value_str)
+
+            if (sqlRes):
+                logger.info("insert{:^25}SUCCESS".format(user_id))
+                ret = SUCCESS
+            else:
+                logger.error(" insert FAIL {}={} ".format("user_id", user_id))
+                ret = FAILURE.setRet(msg=" insert FAIL {}={} ".format("user_id", user_id))
+        else:
+            logger.error("{}={} 已存在".format("user_id", user_id))
+            ret = FAILURE.setRet(msg="{}={} 已存在".format("user_id", user_id))
+
+        return ret
+
+    def getBalance(self, user_id):
+        data = {}
+
+        if self.countUserDB(user_id) == 1:
+            sqlOper = SQLOper()
+            keys = self.select_all_which.replace(" ", "").split(",")
+            res = sqlOper.executeSelectCondition1(self.select_all_which, self.table_name, "user_id", user_id)
+            # balance = Balance(user_id)
+            print(res)
+            for i in range(keys.__len__()):
+                data.setdefault(keys[i], res[0][i])
+            print(data)
+            return SUCCESS.setData(data)
+        else:
+            logger.error("user_id 不存在")
+            return FAILURE.setRet(msg="user_id 不存在")
+
+    def updateBalance(self, user_id, data):
+
+        if self.countUserDB(user_id) == 1:
+            sqlOper = SQLOper()
+            str_update=self.getUpdateStr(data)
+            res=sqlOper.executeSomeUpdateSql(self.table_name,str_update,"user_id",user_id)
+            logger.info("update user_id={} success".format(user_id))
+            return SUCCESS.setRet(msg="update user_id={} success".format(user_id),data={"res",res})
+        else:
+            logger.error("user_id 不存在")
+            return FAILURE.setRet(msg="user_id 不存在")
+
+    def getUpdateStr(self, data):
+        if data.__len__() == 0:
+            return ""
+        str = ""
+        for k, v in data.items():
+            if isinstance(v, int):
+                str = "%s,%s='%d'" % (str, k, v)
+            else:
+                str = "%s,%s='%s'" % (str, k, v)
+
+        return str[1:]
 
 
 class Server:
-
     def __init__(self):
-        self.serverId=""
-        self.ip=""
-        self.port=""
-        self.key=""
-        self.used=False
-        self.crtTime=""
-        self.uptTime=""
+        self.data = {}
+        self.keys_list = ["server_id","port","key","server_status"]
+        self.pattern = ""
+        self.value_str = ""
+        self.flushInsert()
 
-#dataStr="{'xxx':'123','aaa':'334'}"
-    def setServer(self, dataStr):
-        data = {}
-        data = json.loads(dataStr)
+    def setServerDict(self, data):
+        # data={}
+        for key in self.keys_list:
+            if data.__contains__(key) is False:
+                logger.error(" don't have key='{}'".format(key))
+                return FAILURE.setRet(msg=" don't have key='{}'".format(key))
+            self.data[key] = data[key]
+        self.flushInsert()
 
-        if (data.has_key('serverId')):
-            self.serverId = data['serverId']
-        if (data.has_key('ip')):
-            self.ip = data['ip']
-        if (data.has_key('port')):
-            self.port = data['port']
-        if (data.has_key('key')):
-            self.key = data['key']
-        if (data.has_key('used')):
-            self.used = data['used']
-        if (data.has_key('crtTime')):
-            self.crtTime = data['crtTime']
-        if (data.has_key('uptTime')):
-            self.uptTime = data['uptTime']
+    def flushInsert(self):
+        self.pattern = ""
+        self.value_str = ""
+        for k, v in self.data.items():
+            self.pattern = "%s,%s" % (self.pattern, k)
+            if isinstance(v, int):
+                self.value_str = "%s,%d" % (self.value_str, v)
+            else:
+                self.value_str = "%s,'%s'" % (self.value_str, v)
+        self.pattern = self.pattern[1:]
+        self.value_str = self.value_str[1:]
+
+
+def insertServer():
+    server_dao = ServerDao()
+    server = Server("201805020001")
+    data = {"server_id": "10120120325265545", "port": "66545", "key": "PLOKIJKLKK","server_status":"1"}
+    server.setServerDict(data)
+    # data={"user_id":"201805020001"}
+    # print(balance.__dict__)
+    server_dao.insertServer2DB(server)
+
+
+def updateBalance():
+    balance_dao = BalanceDao()
+    balance = Balance()
+    user_id = "201805020002"
+    data = {"current_day": 180, "total_day": 180, "total_balance": 55}
+    balance_dao.updateBalance(user_id, data)
+
+
+def selectBalance():
+    balance_dao = BalanceDao()
+
+    # ret=balance_dao.
+
+
+def getBalance():
+    ret = RetMsg()
+    balance_dao = BalanceDao()
+    # balance = Balance("12355")
+    ret = balance_dao.getBalance("201805020002")
+
+    print(SUCCESS.getCode())
+    print(ret.getCode())
+    if ret.getCode() is SUCCESS.getCode():
+        print(ret.data)
+    else:
+        print(ret)
+
+
+if __name__ == '__main__':
+# insertBalance()
+# getBalance()
+# balance=Balance()
+# balance.setBalanceDict(getBalance())
+# print(balance.data)
+    updateBalance()
--- ladder/dao/Trans.py
+++ ladder/dao/Trans.py
@@ -1,41 +1,64 @@
-
 import json
 import datetime
+from ladder.lib.Logger import Logger
+from ladder.lib.RetMsg import *
+
+logger = Logger("trans").getlog()
 
 class TransDao:
+    """
+     operation the tbl_trans table
+    """
 
     def __init__(self):
-        self.table_name="tbl_trans"
-        self.select_all_which="user_id,buss_no,trans_cd,trans_at,trans_day,settle_dt,curr_day,curr_balance"
+        self.table_name = "tbl_trans"
+        self.select_all_which = "user_id,buss_no,trans_cd,trans_at,trans_day,settle_dt,curr_day,curr_balance"
 
+    # 插入新交易，同步修改余额表
+    def insertTrans(self, trans):
 
+        user_id=trans.data["user_id"]
+        trans_cd=trans.data["trans_cd"]
 
-    #插入新交易，同步修改余额表
-    def insertTrans(self):
-        # TODO
+        sqlOper = SQLOper()
+        sqlRes = sqlOper.executeInsertSql(self.table_name, trans.pattern, trans.value_str)
+        if (sqlRes):
+            logger.info("insert user_id={},trans_cd={} SUCCESS".format(user_id,trans_cd))
+            ret = SUCCESS.setRet(msg="insert user_id={},trans_cd={} SUCCESS".format(user_id,trans_cd))
+        else:
+            logger.error(" insert FAILURE {}={} ".format("user_id", user_id))
+            ret = FAILURE.setRet(msg=" insert FAIL {}={} ".format("user_id", user_id))
 
+    def getTrans(self,data):
 
-        pass
+        cond=""
+        data={}
+        for k,v in data.items():
+            cond=" %s and %s='%s' "%(cond,k,v)
+        sql="select %s from %s where 1=1 %s"%(self.select_all_which,self.table_name,cond)
+        sqlOper = SQLOper()
+        res=sqlOper.executeSql(sql)
 
+        return res
 
 
 class Trans:
-
-    def __init__(self,data={}):
+    def __init__(self, data={}):
         self.data = {}
-        self.keys_list = ["user_id", "buss_no", "trans_cd", "trans_at", "trans_day", "settle_dt","curr_day","curr_balance"]
+        self.keys_list = ["user_id", "buss_no", "trans_cd", "trans_at", "trans_day", "settle_dt", "curr_day",
+                          "curr_balance"]
         self.pattern = ""
         self.value_str = ""
 
         self.setTrans(data)
         self.flushInsert()
 
-    def setTrans(self,data):
-        data={}
+    def setTrans(self, data):
+        data = {}
         for i in range(self.keys_list.__len__()):
-            key=self.key_list[i]
+            key = self.key_list[i]
             if data.__contains__(key):
-                self.data[key]=data[key]
+                self.data[key] = data[key]
 
     def flushInsert(self):
         self.pattern = ""
@@ -49,16 +72,24 @@ class Trans:
         self.pattern = self.pattern[1:]
         self.value_str = self.value_str[1:]
 
+
 def insertTrans():
-    data={"user_id":"201805020002","buss_no":"20180502181423111256","trans_cd":"1002","trans_at":10,
-          "trans_day":30,"settle_dt":"20180502","curr_day":30,"curr_balance":10 }
+    data = {"user_id": "201805020002", "buss_no": "20180502181423111256", "trans_cd": "1002", "trans_at": 10,
+            "trans_day": 30, "settle_dt": "20180502", "curr_day": 30, "curr_balance": 10}
 
-    trans=Trans(data)
+    trans = Trans(data)
 
-    trans_dao= TransDao()
+    trans_dao = TransDao()
     trans_dao.insertTrans(trans)
 
 
-if __name__=='__main__':
+def selectTrans():
+    data = {"buss_no": "12345679", "trans_cd": "1002", "user_id": "20180502111"}
 
+    trans_dao = TransDao()
+    res= trans_dao.getTrans(data)
+    print(res)
+
+
+if __name__ == '__main__':
     insertTrans()
